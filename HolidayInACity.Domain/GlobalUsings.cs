1e3ac2706cce0407559948e0935db189f4ad81d5
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2023-08-15 18:02:44 +0200

    Add configuration to entities

--- HolidayInACity.Domain/GlobalUsings.cs
+++ HolidayInACity.Domain/GlobalUsings.cs
@@ -1,1 +1,3 @@
-﻿global using System.ComponentModel.DataAnnotations;
+﻿global using Microsoft.EntityFrameworkCore;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;

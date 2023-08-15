e7635a0e91e9b1d8f3ab8711f55ad7c69dd813d4
Radek-Szczepanik <57062649+Radek-Szczepanik@users.noreply.github.com>
2023-08-15 18:04:25 +0200

    Add configuration to entities (#2)

---
+++ HolidayInACity.Domain/Entities/Configurations/EventOrganizerConfiguration.cs
@@ -0,0 +1,26 @@
+﻿namespace HolidayInACity.Domain.Entities.Configurations;
+
+public class EventOrganizerConfiguration : IEntityTypeConfiguration<EventOrganizer>
+{
+    public void Configure(EntityTypeBuilder<EventOrganizer> builder)
+    {
+        builder.Property(x => x.Name)
+               .IsRequired()
+               .HasMaxLength(100);
+
+        builder.HasMany(x => x.HolidayEvents)
+               .WithOne(y => y.EventOrganizer)
+               .HasForeignKey(z => z.EventOrganizerId)
+               .IsRequired();
+
+        builder.HasOne(x => x.EventOrganizerAddress)
+               .WithOne(y => y.EventOrganizer)
+               .HasForeignKey<EventOrganizerAddress>(z => z.Id)
+               .IsRequired();
+
+        builder.HasOne(x => x.EventOrganizerContact)
+               .WithOne(y => y.EventOrganizer)
+               .HasForeignKey<EventOrganizerContact>(z => z.Id)
+               .IsRequired();
+    }
+}

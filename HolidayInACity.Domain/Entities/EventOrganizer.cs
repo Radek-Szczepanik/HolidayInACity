a87b7fbcedf980255399e50f62609be980cc12ea
Radek-Szczepanik <radekszczepanik.rsz@gmail.com>
2023-08-15 17:32:48 +0200

    Add entities

---
+++ HolidayInACity.Domain/Entities/EventOrganizer.cs
@@ -0,0 +1,12 @@
+﻿namespace HolidayInACity.Domain.Entities;
+
+public class EventOrganizer : EntityBase
+{
+    public string Name { get; set; } = default!;
+
+    public int EventOrganizerAddressId { get; set; }
+    public virtual EventOrganizerAddress EventOrganizerAddress { get; set; } = default!;
+    public int EventOrganizerContactId { get; set; }
+    public virtual EventOrganizerContact EventOrganizerContact { get; set; } = default!;
+    public virtual ICollection<HolidayEvent> HolidayEvents { get; } = new List<HolidayEvent>();
+}
---
+++ HolidayInACity.Domain/Entities/EventOrganizerContact.cs
@@ -0,0 +1,9 @@
+﻿namespace HolidayInACity.Domain.Entities;
+
+public class EventOrganizerContact : EntityBase
+{
+    public string PhoneNumber { get; set; } = default!;
+    public string Email { get; set; } = default!;
+
+    public virtual EventOrganizer EventOrganizer { get; set; } = default!;
+}
